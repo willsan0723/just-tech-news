40cbc32283fedfaccbcca2c3ae5ce9f2c106bdf3
willsan0723 <william.santee@gmail.com>
2020-10-22 14:53:44 -0600

    finalized password validation

--- models/User.js
+++ models/User.js
@@ -2,8 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-// create our User model
-class User extends Model { }
+// create our user model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
@@ -53,5 +58,6 @@ User.init(
         modelName: 'user'
     }
 );
+
 
 module.exports = User;
